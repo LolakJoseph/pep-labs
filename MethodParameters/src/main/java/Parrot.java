59d5699da6d14c6bfd88b0d5e05f3881d14377e9
LolakJoseph <lolakjoseph86@gmail.com>
2022-12-25 01:25:44 +0000

    Saved progress on MethodParameters 2022-12-25 01:25:44 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -1,7 +1,5 @@
 
 public class Parrot {
-    private String text;
-
     /**
      * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
      * instead of returning "return text please!"
@@ -14,7 +12,7 @@ public class Parrot {
      * @return text.
      */
     public String copy(String text){
-        return "polly"
+        return text
         ;
     }
 }
